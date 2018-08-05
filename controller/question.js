99da872e78cc1b48cc7916b947804ba0ee5063dd
eveliao <liaoqz@outlook.com>
2018-08-05 11:15:59 +0800

    clean code

--- controller/question.js
+++ controller/question.js
@@ -17,18 +17,16 @@ const generateQuestion = async(ctx)=>{
         let segmentObj = await SegmentModel.findById(segmentId).exec();
         let segment = segmentObj["content"]
         
-
         let learningWordsId = ctx.request.body.words;
         let learningWordsInfo={};
         for(let i=0;i<learningWordsId.length;i++){
             try{
                 let tempWordId = learningWordsId[i];
                 let wordInfo = await WordModel.findById(tempWordId).exec();
-
                 let word = wordInfo["word"];
                 let explanations = wordInfo["chineseExplanations"];
                 let posList = [];
-                for(let j = 0; j < explanations.length; ++j){
+                for(let j = 0; j < explanations.length; j++){
                     let pos = explanations[j]["pos"];
                     posList.push(pos);
                 }
@@ -42,7 +40,6 @@ const generateQuestion = async(ctx)=>{
         let token = jwt.getToken(ctx);
         let userId = token.id;
         let user = await UserModel.findById(userId).exec();
-        
         let learntWordsId = user["words"];
         let learntWordsInfo = {};
         for(let i=0;i<learntWordsId.length;i++){
@@ -52,7 +49,7 @@ const generateQuestion = async(ctx)=>{
                 let word = wordInfo["word"];
                 let explanations = wordInfo["chineseExplanations"];
                 let posList = [];
-                for(let j = 0; j < explanations.length; ++j){
+                for(let j = 0; j < explanations.length; j++){
                     let pos = explanations[j]["pos"];
                     posList.push(pos);
                 }
@@ -65,10 +62,6 @@ const generateQuestion = async(ctx)=>{
         let altWordsInfo = {};
         if(learntWordsId.length < 300){
             let userLevel = user["level"] > 1 ? user["level"] - 1 : 1;
-            // info.push({
-            //     "userLevel": userLevel,
-            //     "orgLevel": user["level"]
-            // });
             let altWordsRawInfo = await WordModel.find({"level":userLevel}).exec();
             for(let i = 0; i < altWordsRawInfo.length; i++){
                 try{
@@ -88,29 +81,11 @@ const generateQuestion = async(ctx)=>{
             }            
         }
 
-        let endTime = new Date();
-        let timeDiff = endTime - startTime; //in ms
-        timeDiff /= 1000;
-
-        let elapsedTime = []
-        elapsedTime.push(timeDiff);
-
-
-
-        // info.push(altWordsInfo.length);
-        // info.push(learntWordsInfo.length);
-
-        startTime = new Date();
-
         var java = require("java");
         java.classpath.push(path.resolve(__dirname, './src'));
         java.classpath.push(path.resolve(__dirname, './src/lib/stanford-ner-3.4.1.jar'));
         java.classpath.push(path.resolve(__dirname, './src/lib/stanford-postagger-3.4.1.jar'));
         java.classpath.push(path.resolve(__dirname, './src/lib/gson-2.8.5.jar'));
-        
-        // var runInterface = java.newInstanceSync("generator.Test");
-        // java.callMethodSync(runInterface, "getList", "{\"id\":\"abc\"}");
-
         var runInterface = java.newInstanceSync("generator.QGen_server");
         java.callMethodSync(runInterface, "preprocess", 
                             JSON.stringify(segment), 
@@ -119,9 +94,7 @@ const generateQuestion = async(ctx)=>{
                             JSON.stringify(altWordsInfo));
         let resultStr = java.callMethodSync(runInterface, "getResultJson");
         let resultArr = JSON.parse(resultStr);
-
         let questions = []
-
         for(let i = 0; i < resultArr.length; i++){
             try{
                 let question = resultArr[i][0];
@@ -139,44 +112,15 @@ const generateQuestion = async(ctx)=>{
                 continue;
             }
         }
-        // for(var i = 0; i < resultArr.length; i++) {
-        //     try{
-        //         l
-        //         for(var j = 0; j < resultArr[i].length; j++) {
-        //             info.push(resultArr[i][j]);
-        //         }
-        //     }
-        // }
         let num = questions.length > 10 ? 10 : questions.length;
         let result = commonFunction.getRandomArrayElement(questions, num);
 
-        endTime = new Date();
-        timeDiff = endTime - startTime; //in ms
-        timeDiff /= 1000;
-
-        elapsedTime.push(timeDiff);
-
         ctx.body = result;
-        
-        // ctx.body = {"result": result, "info": info};
-        // ctx.body = {"status":"ok"};
-        // ctx.body = {"questions":questions, 
-        // "first len":resultArr[0].length, 
-        // "first":resultArr[0],
-        // "first q":resultArr[0][0],
-        // "info": info,
-        // "resultArr":resultArr.length
-        // };
         ctx.status = 200;
     }
     catch (err) {
         ctx.status = 400;
-        ctx.body = {"error": "what"};
-        // ctx.body = {error:"error"}
-        // ctx.body = {error:err}
-        // console.log("err");
-        // console.log(err);
-
+        ctx.body = {error:"error"}
     }
 };
 
